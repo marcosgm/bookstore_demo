839a764056f8903edf67386f5146fccc6691027e
Marc Garcia (Consultant) <marc.garcia-ext@mcn.gouv.qc.ca>
2023-08-29 10:59:35 -0400

    version bump

--- main.tf
+++ main.tf
@@ -17,12 +17,12 @@ terraform {
 
 module "bookstore-aws" {
   source  = "app.terraform.io/nimbusworld-demo/bookstore-aws/aws"
-  version = "1.0.2"
+  version = "1.0.3"
   bookstore_title = "AWS Bookstore in PROD"
 }
 
 module "bookstore-azure" {
   source  = "app.terraform.io/nimbusworld-demo/bookstore-azure/azurerm"
-  version = "1.0.1"
+  version = "1.0.2"
   env_vars = { bookstore_title:  "Azure bookstore in PROD" }
 }
